526f785b3508cf2ffb29048c89de05c3928312a0
hoers <hoers@localhost.localdomain>
2017-02-13 21:47:26 -0500

    fixed contour_integral; added variant for inputs in meters

--- contour_integral_meters.m
+++ contour_integral_meters.m
@@ -1,7 +1,7 @@
-function result = contour_integral(freq, x_in, y_in, x1, x2, y1, y2, func)
+function result = contour_integral_meters(freq, x_in, y_in, x1, x2, y1, y2, func)
 
 line_length = sqrt((x2 - x1).^2 + (y2 - y1).^2);
-wavelen = (3e+8)./(freq./1000);
+wavelen = (3e+8)./(freq);
 ang_freq = 2.*pi.*freq;
 k_val = 2.*pi./wavelen;
 mu_nought = pi.*4e-7;
@@ -11,12 +11,12 @@ int_func = func;
 weights = [0.210469457918546e-1, 0.130705540744447e+0, 0.289702301671314e+0, 0.350220370120399e+0, 0.208324841671986e+0];
 nodes = [0.565222820508010e-2, 0.734303717426523e-1, 0.284957404462558e+0, 0.619482264084778e+0, 0.915758083004698e+0];
 
-const_term = -1.*ang_freq.*mu_nought.*line_length./4000;
+const_term = -1.*ang_freq.*mu_nought.*line_length./4;
 
 sum = 0;
 
 for i=1:5
- sum = sum + weights(i).*func(k_val.*sqrt((x_in - 100 - 22.*nodes(i)).^2 + (y_in - 100 + 22.*nodes(i)).^2));
+ sum = sum + weights(i).*func(k_val.*sqrt((x_in - 0.1 - 0.022.*nodes(i)).^2 + (y_in - 0.1 + 0.022.*nodes(i)).^2));
 endfor
 
 result = const_term.*sum;
