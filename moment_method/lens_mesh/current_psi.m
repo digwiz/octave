2cc3490ad773fa1fd91d45b9624ad5a390e58e66
Trevor <tvgamblin@gmail.com>
2017-04-07 20:03:32 -0400

    further cleaned directory

--- moment_method/lens_mesh/current_psi.m
+++
@@ -1,8 +0,0 @@
-function result = current_psi(e_r,F,a,b)
-n = sqrt(e_r);
-numerator = (a.^2).*n-2.*a.*b.*n+a.*F.*sqrt(n-1)+(b.^2).*n-b.*F.*sqrt(n-1);
-denominator = (a.^2).*(n.^2)-2.*a.*b.*(n.^2)+(b.^2).*(n.^2)+(F.^2).*(-1.*n)+(F.^2);
-
-result = acos(numerator./denominator);
-
-end
--- moment_method/lens_mesh/lens_rho.m
+++
@@ -1,8 +0,0 @@
-function result = lens_rho(psi, epsilon_r, focal_length)
-
-numerator = (sqrt(epsilon_r)-1).*focal_length;
-denominator = sqrt(epsilon_r).*cos(psi)-1;
-
-result = numerator./denominator;
-
-end
